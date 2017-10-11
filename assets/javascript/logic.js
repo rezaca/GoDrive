805265a6aa50030830f51892cf3326390922c490
Preston Ross <prestonross77@gmail.com>
2017-10-11 16:06:19 -0400

    skeleton

---
+++ assets/javascript/logic.js
@@ -0,0 +1,17 @@
+
+// var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyB0HU5xMTp4T0uJJB2XBHsPeh1OWxAmBg4&callback=initMap";
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).done(function (response) {
+//     console.log(response);
+// });
+
+// var map;
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: { lat: -34.397, lng: 150.644 },
+//         zoom: 8
+//     });
+// }
