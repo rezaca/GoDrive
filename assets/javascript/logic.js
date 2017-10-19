1fa9cebccaaeef91247f11f29120b7b4207e8c1a
Preston Ross <prestonross77@gmail.com>
2017-10-19 15:43:09 -0400

    center buttons

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -191,7 +191,17 @@ function onSignIn(googleUser) {
     $("#userDisplay").append("<img class='userImg'>" + profile.getImageUrl());
     $("#userDisplay").append(profile.getEmail());
 
+    // auth2 is initialized with gapi.auth2.init() and a user is signed in.
 
+    if (auth2.isSignedIn.get()) {
+        var profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    }
 }
 
 $("#signOut").on("click", function (event) {
@@ -200,18 +210,6 @@ $("#signOut").on("click", function (event) {
         console.log('User signed out.');
     });
 });
-
-// auth2 is initialized with gapi.auth2.init() and a user is signed in.
-
-if (auth2.isSignedIn.get()) {
-    var profile = auth2.currentUser.get().getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-}
 
 // Button for adding users account
 // $("#add-account").on("click", function (event) {
