0794fc4336f96429263306bc6b10f6dfd27d8bd5
Preston Ross <prestonross77@gmail.com>
2017-10-19 15:37:01 -0400

    center buttons again

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -187,9 +187,9 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-    $("#userDisplay").html(profile.getName());
-    $("#userDisplay").html("<img class='userImg'>" + profile.getImageUrl());
-    $("#userDisplay").html(profile.getEmail());
+    $("#userDisplay").append(profile.getName());
+    $("#userDisplay").append("<img class='userImg'>" + profile.getImageUrl());
+    $("#userDisplay").append(profile.getEmail());
 
 
 }
