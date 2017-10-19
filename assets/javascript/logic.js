04d356aa9d68c7724c2fe2d620be3a4e3f5e927b
Preston Ross <prestonross77@gmail.com>
2017-10-19 16:52:00 -0400

    some changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -191,19 +191,6 @@ function onSignIn(googleUser) {
     $("#userDisplay").html("<img class='userImg'>" + profile.getImageUrl());
     $("#userDisplay").html(profile.getEmail());
 
-    // auth2 is initialized with gapi.auth2.init() and a user is signed in.
-
-    gapi.auth2.init();
-
-    if (auth2.isSignedIn.get()) {
-        var profile = auth2.currentUser.get().getBasicProfile();
-        console.log('ID: ' + profile.getId());
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-    }
 }
 
 $("#signOut").on("click", function (event) {
